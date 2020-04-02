6b4b9e697d792f49a3afd8ff086428b7ee39496c
sowamy04 <amysow04@gmail.com>
2020-04-02 22:11:58 +0000

    correction 4

--- Exercice4/Exercice4.php
+++ Exercice4/Exercice4.php
@@ -6,8 +6,8 @@
 </head>
 <body>
 	<form method="POST">
-		Saisissez un paragraphe SVP! <br/><br/>
-		<textarea type="submit" name="pg" cols="60" rows="10"></textarea> <br/><br/>
+		Saisissez un texte SVP! <br/><br/>
+		<textarea type="submit" name="pg" cols="60" rows="10" ><?= filter_input(INPUT_POST, 'pg') ?></textarea> <br/><br/>
 		<button type="submit" name="bouton" style=" width: 10%; height: 30px; background-color: blue; color: white; margin-left: 10%;"> Soumettre</button>
 	</form>
 	<br/> <br/>
@@ -25,8 +25,8 @@
 		for ($i=0; $i <count($tab) ; $i++) { 
 			$tab[$i] = trim($tab[$i]);
 		}
-		echo "les phrases du paragraphe sont: <br/>";
-		var_dump($tab);
+		/*echo "les phrases du paragraphe sont: <br/>";
+		var_dump($tab);*/
 	}
 
 	include ("fonction4.php");
@@ -41,7 +41,16 @@
 		$tabValid[$i] = preg_replace('#[\s]+#', ' ', $tabValid[$i]);
 	}
 	echo "les phrases valides sont: <br/>";
-	var_dump($tabValid);
+	?>
+	<textarea cols="60" rows="10" readonly="readonly"> 
+		<?php
+			for ($i=0; $i < count($tabValid) ; $i++) { 
+				echo $tabValid[$i];
+				echo "\n"; echo "\n";
+			}
+		?> </textarea>
+
+	<?php
 
 	}
 
