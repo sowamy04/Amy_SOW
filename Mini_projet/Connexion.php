8757a971781f497e9d065f37af8b55c5bc790dd0
sowamy04 <amysow04@gmail.com>
2020-04-10 14:20:33 +0000

    first commit mini projet

---
+++ Mini_projet/Connexion.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Connexion </title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="CSS/Connexion.css">
+</head>
+<body>
+
+	<div>
+		
+		<div class="header"> 
+
+			<div class="logo"> <img src="Images/logo-QuizzSA.png"> </div>
+			<div class="definition"> Le plaisir de jouer </div>
+
+		</div>
+
+		<div class="middle">
+			
+			<div class="con">
+				
+				<div class="loginForm"> Login Form</div>
+				<div class="intCon"> 
+
+						<div class="username">
+							<input type="text" name="login" placeholder="Login" class="logPass">
+							<img src="Images/Icones/icone-user.png" class="iconCon"> 
+						</div>
+
+						<br/> <br/>
+
+						<div class="pass">
+							<input type="password" name="pass" placeholder="Password" class="logPass"> 
+							<img src="Images/Icones/icone-password.png" class="iconCon">
+						</div>
+
+						<br/> <br/>
+
+						<div>
+							
+							<button id="connexion"> Connexion</button>
+							<button id="inscription"> S'inscrire pour jouer? </button>
+
+						</div>
+
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+</body>
+</html>
